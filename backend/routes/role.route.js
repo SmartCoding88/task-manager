c75b2f65e180e8aca09bb69d46e66a26124c4ef9
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-17 16:42:26 +0100

    Epic1- REST API

--- backend/routes/role.route.js
+++ backend/routes/role.route.js
@@ -2,6 +2,34 @@ const express = require('express');
 const route = express.Router();
 const Role = require('../models/role.model');
 
+
+//retrieve roles list
+route.get("/",(req, res)=>{
+    Role.find({})
+    .then((roles)=>{
+        console.log("Roles", roles);
+        res.status(200).json(roles);
+    })
+    .catch((err)=>{
+        console.log("No roles were found!!", err);
+        res.status(400).json({message: "No roles were found!!"});
+    });
+
+});
+
+//get role by ID
+route.get("/:id",(req, res)=>{
+    Role.findById(req.params.id)
+    .then((role)=>{
+        console.log("Role: ",role)
+        res.status(200).json(role)
+    })
+    .catch((err)=>{
+        console.log("No role was found!!", err);
+        res.status(400).json({message: "No role was found!!"});
+    });
+});
+
 //create new Role
 route.post("/", (req, res)=>{
     let roleInfo = req.body
@@ -15,6 +43,7 @@ route.post("/", (req, res)=>{
     })
     .catch((err)=>{
         console.log("Unable to create new role", err);
-    })
-})
+    });
+});
+
 module.exports = route;
